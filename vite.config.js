68670777526cecc539cf994cca27dc132f77149d
Meseten <benjamesduag.edu@gmail.com>
2025-11-02 12:25:16 +0800

    Configure project for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AGWA-Water-Services-Inc./',
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+})
