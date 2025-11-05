a58db6286b6cf024d7b7b22940770b5237d69c1d
Meseten <benjamesduag.edu@gmail.com>
2025-11-05 17:12:38 +0800

    fix:final na final na final na final na final ayoko na talaga attempt security and formatting errors

--- src/firebase/firestorePaths.js
+++ src/firebase/firestorePaths.js
@@ -1,17 +1,48 @@
-export const usersCollectionPath = () => `users`;
-export const userProfileDocumentPath = (userId) => `${usersCollectionPath()}/${userId}/profile/data`;
-
-export const publicDataCollectionPath = () => `public/data`;
-export const profilesCollectionPath = () => `${publicDataCollectionPath()}/profiles`;
-export const supportTicketsCollectionPath = () => `${publicDataCollectionPath()}/support_tickets`;
-export const supportTicketDocumentPath = (ticketId) => `${supportTicketsCollectionPath()}/${ticketId}`;
-export const announcementsCollectionPath = () => `${publicDataCollectionPath()}/announcements`;
-export const announcementDocumentPath = (announcementId) => `${announcementsCollectionPath()}/${announcementId}`;
-export const systemSettingsDocumentPath = () => `${publicDataCollectionPath()}/system_config/settings`;
-export const allBillsCollectionPath = () => `${publicDataCollectionPath()}/all_bills`;
-export const allBillDocumentPath = (billId) => `${allBillsCollectionPath()}/${billId}`;
-export const allMeterReadingsCollectionPath = () => `${publicDataCollectionPath()}/all_meter_readings`;
-export const allMeterReadingDocumentPath = (readingId) => `${allMeterReadingsCollectionPath()}/${readingId}`;
-export const meterRoutesCollectionPath = () => `${publicDataCollectionPath()}/meter_routes`;
-export const serviceInterruptionsCollectionPath = () => `${publicDataCollectionPath()}/service_interruptions`;
-export const serviceInterruptionDocumentPath = (id) => `${serviceInterruptionsCollectionPath()}/${id}`;
+export function usersCollectionPath() {
+    return `users`;
+}
+export function userProfileDocumentPath(userId) {
+    return `${usersCollectionPath()}/${userId}/profile/data`;
+}
+export function publicDataCollectionPath() {
+    return `public/data`;
+}
+export function profilesCollectionPath() {
+    return `${publicDataCollectionPath()}/profiles`;
+}
+export function supportTicketsCollectionPath() {
+    return `${publicDataCollectionPath()}/support_tickets`;
+}
+export function supportTicketDocumentPath(ticketId) {
+    return `${supportTicketsCollectionPath()}/${ticketId}`;
+}
+export function announcementsCollectionPath() {
+    return `${publicDataCollectionPath()}/announcements`;
+}
+export function announcementDocumentPath(announcementId) {
+    return `${announcementsCollectionPath()}/${announcementId}`;
+}
+export function systemSettingsDocumentPath() {
+    return `${publicDataCollectionPath()}/system_config/settings`;
+}
+export function allBillsCollectionPath() {
+    return `${publicDataCollectionPath()}/all_bills`;
+}
+export function allBillDocumentPath(billId) {
+    return `${allBillsCollectionPath()}/${billId}`;
+}
+export function allMeterReadingsCollectionPath() {
+    return `${publicDataCollectionPath()}/all_meter_readings`;
+}
+export function allMeterReadingDocumentPath(readingId) {
+    return `${allMeterReadingsCollectionPath()}/${readingId}`;
+}
+export function meterRoutesCollectionPath() {
+    return `${publicDataCollectionPath()}/meter_routes`;
+}
+export function serviceInterruptionsCollectionPath() {
+    return `${publicDataCollectionPath()}/service_interruptions`;
+}
+export function serviceInterruptionDocumentPath(id) {
+    return `${serviceInterruptionsCollectionPath()}/${id}`;
+}
