4547ca1314911907866be52ee1c99df4ae474217
Meseten <benjamesduag.edu@gmail.com>
2025-11-15 11:55:58 +0800

    fix: update point expiration policy in RebateProgramSection for clarity; add missing imports in firebaseConfig

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,7 +5,7 @@ import {
     doc, setDoc, getDoc, addDoc, collection, updateDoc,
     deleteDoc, query, where, getDocs, serverTimestamp,
     Timestamp, orderBy, writeBatch, getCountFromServer, arrayUnion, limit,
-    FieldPath, documentId, startAfter 
+    FieldPath, documentId, startAfter, runTransaction, FieldValue
 } from 'firebase/firestore';
 import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
@@ -60,5 +60,5 @@ export {
     doc, setDoc, getDoc, addDoc, collection, updateDoc,
     deleteDoc, query, where, getDocs, serverTimestamp,
     Timestamp, orderBy, writeBatch, getCountFromServer, arrayUnion, limit,
-    FieldPath, documentId, startAfter
+    FieldPath, documentId, startAfter, runTransaction, FieldValue
 };
